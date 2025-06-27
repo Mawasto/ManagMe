5ac87ad636e21e35f20fdd2db8c2aab0e4216cb4
Kacper Mitana <mitana.kacper1@gmail.com>
2025-06-27 21:04:56 +0200

    lab2 cont - mock logged user jan kowalski

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { UserManager } from '../api/UserManager';
+import React from "react";
+import { UserManager } from "../api/UserManager";
 
 const UserInfo: React.FC = () => {
-  const user = UserManager.getLoggedInUser();
+    const user = UserManager.getLoggedInUser();
 
-  return (
-    <div>
-      <h2>Logged-in User</h2>
-      <p>
-        {user.firstName} {user.lastName}
-      </p>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Logged-in User</h2>
+            <p>
+                {user.firstName} {user.lastName}
+            </p>
+        </div>
+    );
 };
 
 export default UserInfo;
